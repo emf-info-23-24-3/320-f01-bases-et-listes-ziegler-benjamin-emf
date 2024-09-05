0c08e9e5c1dd34c243e9c2b2257c751e8938ab70
Benjamin <benjamin.ziegler@studentfr.ch>
2024-09-05 08:50:23 +0200

    éval formative

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,15 +1,18 @@
 package app;
-import java.util.List;
 import java.util.ArrayList;
+ 
 public class Application {
-
+ 
     public static void main(String[] args) {
-
-        List<Personne> listPersonne = new ArrayList<>();
-
-        for (Personne i : listPersonne) {
-            
+        ArrayList<Personne> personnes = new ArrayList<>();
+ 
+        personnes.add(new Personne("Benjamin", "Ziegler", "rue des fontanettes", "Avry", 1633));
+        personnes.add(new Personne("Lebron", "James", "rue des fontanettes2", "Los Angeles", 16332));
+ 
+        for (Personne personne : personnes) {
+            System.out.println(personne.getNom());
         }
+ 
     }
-
+ 
 }
